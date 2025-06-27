e6e0f6b3b7d78cc6c54fcd043d5ee15028aff3c1
Kgothatso Masekwameng <166397326+Kgothatso-Masekwameng@users.noreply.github.com>
2025-06-27 13:45:16 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SchedetBot
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage chatTab;
+        private System.Windows.Forms.TabPage quizTab;
+        private System.Windows.Forms.TabPage taskTab;
+        private System.Windows.Forms.TabPage reminderTab;
+
+        private System.Windows.Forms.TextBox chatbotTextBox;
+        private System.Windows.Forms.RichTextBox chatbotOutput;
+        private System.Windows.Forms.Button chatbotSendButton;
+
+        private System.Windows.Forms.Label quizLabel;
+        private System.Windows.Forms.RadioButton quizOption1;
+        private System.Windows.Forms.RadioButton quizOption2;
+        private System.Windows.Forms.RadioButton quizOption3;
+        private System.Windows.Forms.RadioButton quizOption4;
+        private System.Windows.Forms.Button submitQuizButton;
+        private System.Windows.Forms.ListBox questionSummaryListBox;
+
+        private System.Windows.Forms.TextBox taskNameTextBox;
+        private System.Windows.Forms.TextBox taskDescTextBox;
+        private System.Windows.Forms.TextBox taskTimeTextBox;
+        private System.Windows.Forms.ComboBox taskStatusComboBox;
+        private System.Windows.Forms.ListBox taskListBox;
+        private System.Windows.Forms.Button addTaskButton;
+        private System.Windows.Forms.Button editTaskButton;
+        private System.Windows.Forms.Button deleteTaskButton;
+
+        private System.Windows.Forms.TextBox reminderTextBox;
+        private System.Windows.Forms.Button setReminderButton;
+        private System.Windows.Forms.Timer reminderTimer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null)
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.chatTab = new System.Windows.Forms.TabPage() { Text = "Chatbot" };
+            this.quizTab = new System.Windows.Forms.TabPage() { Text = "Cyber Quiz" };
+            this.taskTab = new System.Windows.Forms.TabPage() { Text = "Task Manager" };
+            this.reminderTab = new System.Windows.Forms.TabPage() { Text = "Reminders" };
+
+            // TabControl
+            this.tabControl.Controls.Add(this.chatTab);
+            this.tabControl.Controls.Add(this.quizTab);
+            this.tabControl.Controls.Add(this.taskTab);
+            this.tabControl.Controls.Add(this.reminderTab);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            // DARK THEME COLORS
+            var backDark = System.Drawing.Color.FromArgb(30, 30, 30);
+            var panelDark = System.Drawing.Color.FromArgb(45, 45, 48);
+            var fore = System.Drawing.Color.White;
+            var buttonDark = System.Drawing.Color.FromArgb(60, 60, 60);
+
+            // CHAT TAB
+            this.chatbotOutput = new System.Windows.Forms.RichTextBox
+            {
+                Location = new System.Drawing.Point(10, 10),
+                Size = new System.Drawing.Size(640, 250),
+                ReadOnly = true,
+                BackColor = backDark,
+                ForeColor = fore,
+                BorderStyle = System.Windows.Forms.BorderStyle.None
+            };
+            this.chatbotTextBox = new System.Windows.Forms.TextBox
+            {
+                Location = new System.Drawing.Point(10, 270),
+                Width = 500,
+                BackColor = panelDark,
+                ForeColor = fore
+            };
+            this.chatbotSendButton = new System.Windows.Forms.Button
+            {
+                Text = "Send",
+                Location = new System.Drawing.Point(520, 270),
+                BackColor = buttonDark,
+                ForeColor = fore,
+                FlatStyle = System.Windows.Forms.FlatStyle.Flat
+            };
+            this.chatbotSendButton.Click += new System.EventHandler(this.chatbotSendButton_Click);
+            this.chatTab.BackColor = panelDark;
+            this.chatTab.Controls.AddRange(new System.Windows.Forms.Control[] { chatbotOutput, chatbotTextBox, chatbotSendButton });
+
+            // QUIZ TAB
+            this.quizLabel = new System.Windows.Forms.Label { Text = "Question", Location = new System.Drawing.Point(10, 10), Width = 600, ForeColor = fore };
+            this.quizOption1 = new System.Windows.Forms.RadioButton { Location = new System.Drawing.Point(30, 40), Width = 600, ForeColor = fore };
+            this.quizOption2 = new System.Windows.Forms.RadioButton { Location = new System.Drawing.Point(30, 70), Width = 600, ForeColor = fore };
+            this.quizOption3 = new System.Windows.Forms.RadioButton { Location = new System.Drawing.Point(30, 100), Width = 600, ForeColor = fore };
+            this.quizOption4 = new System.Windows.Forms.RadioButton { Location = new System.Drawing.Point(30, 130), Width = 600, ForeColor = fore };
+            this.submitQuizButton = new System.Windows.Forms.Button
+            {
+                Text = "Submit",
+                Location = new System.Drawing.Point(30, 170),
+                BackColor = buttonDark,
+                ForeColor = fore,
+                FlatStyle = System.Windows.Forms.FlatStyle.Flat
+            };
+            this.submitQuizButton.Click += new System.EventHandler(this.submitQuizButton_Click);
+            this.questionSummaryListBox = new System.Windows.Forms.ListBox
+            {
+                Location = new System.Drawing.Point(400, 10),
+                Size = new System.Drawing.Size(240, 150),
+                BackColor = panelDark,
+                ForeColor = fore
+            };
+            this.quizTab.BackColor = panelDark;
+            this.quizTab.Controls.AddRange(new System.Windows.Forms.Control[] {
+                quizLabel, quizOption1, quizOption2, quizOption3, quizOption4, submitQuizButton, questionSummaryListBox
+            });
+
+            // TASK TAB
+            this.taskNameTextBox = new System.Windows.Forms.TextBox { PlaceholderText = "Task Name", Location = new System.Drawing.Point(10, 10), BackColor = panelDark, ForeColor = fore };
+            this.taskDescTextBox = new System.Windows.Forms.TextBox { PlaceholderText = "Task Description", Location = new System.Drawing.Point(10, 40), BackColor = panelDark, ForeColor = fore };
+            this.taskTimeTextBox = new System.Windows.Forms.TextBox { PlaceholderText = "Time", Location = new System.Drawing.Point(10, 70), BackColor = panelDark, ForeColor = fore };
+            this.taskStatusComboBox = new System.Windows.Forms.ComboBox
+            {
+                Location = new System.Drawing.Point(10, 100),
+                Width = 200,
+                BackColor = panelDark,
+                ForeColor = fore,
+                DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+            };
+            this.taskStatusComboBox.Items.AddRange(new string[] { "Pending", "In Progress", "Completed" });
+
+            this.taskListBox = new System.Windows.Forms.ListBox
+            {
+                Location = new System.Drawing.Point(250, 10),
+                Size = new System.Drawing.Size(300, 150),
+                BackColor = panelDark,
+                ForeColor = fore
+            };
+
+            this.addTaskButton = new System.Windows.Forms.Button { Text = "Add", Location = new System.Drawing.Point(10, 130), BackColor = buttonDark, ForeColor = fore, FlatStyle = System.Windows.Forms.FlatStyle.Flat };
+            this.editTaskButton = new System.Windows.Forms.Button { Text = "Edit", Location = new System.Drawing.Point(90, 130), BackColor = buttonDark, ForeColor = fore, FlatStyle = System.Windows.Forms.FlatStyle.Flat };
+            this.deleteTaskButton = new System.Windows.Forms.Button { Text = "Delete", Location = new System.Drawing.Point(170, 130), BackColor = buttonDark, ForeColor = fore, FlatStyle = System.Windows.Forms.FlatStyle.Flat };
+
+            this.addTaskButton.Click += new System.EventHandler(this.addTaskButton_Click);
+            this.editTaskButton.Click += new System.EventHandler(this.editTaskButton_Click);
+            this.deleteTaskButton.Click += new System.EventHandler(this.deleteTaskButton_Click);
+            this.taskListBox.SelectedIndexChanged += new System.EventHandler(this.taskListBox_SelectedIndexChanged);
+            this.taskTab.BackColor = panelDark;
+            this.taskTab.Controls.AddRange(new System.Windows.Forms.Control[] {
+                taskNameTextBox, taskDescTextBox, taskTimeTextBox, taskStatusComboBox,
+                taskListBox, addTaskButton, editTaskButton, deleteTaskButton
+            });
+
+            // REMINDER TAB
+            this.reminderTextBox = new System.Windows.Forms.TextBox
+            {
+                PlaceholderText = "Enter reminder (e.g., Drink water at 15:30)",
+                Location = new System.Drawing.Point(10, 10),
+                Width = 400,
+                BackColor = panelDark,
+                ForeColor = fore
+            };
+            this.setReminderButton = new System.Windows.Forms.Button
+            {
+                Text = "Set Reminder",
+                Location = new System.Drawing.Point(420, 10),
+                BackColor = buttonDark,
+                ForeColor = fore,
+                FlatStyle = System.Windows.Forms.FlatStyle.Flat
+            };
+            this.setReminderButton.Click += new System.EventHandler(this.SetReminderButton_Click);
+            this.reminderTimer = new System.Windows.Forms.Timer(this.components)
+            {
+                Interval = 60000 // 1 minute
+            };
+            this.reminderTimer.Tick += new System.EventHandler(this.ReminderTimer_Tick);
+            this.reminderTab.BackColor = panelDark;
+            this.reminderTab.Controls.AddRange(new System.Windows.Forms.Control[] { reminderTextBox, setReminderButton });
+
+            // FINALIZE FORM
+            this.BackColor = backDark;
+            this.Controls.Add(this.tabControl);
+            this.Text = "Schedet";
+            this.ClientSize = new System.Drawing.Size(700, 400);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SchedetBot
+{
+    public partial class Form1 : Form
+    {
+        private string userName = "User";
+        private int currentQuestion = 0;
+        private int score = 0;
+        private List<(string time, string message)> reminders = new();
+        private List<string> activityLog = new();
+        private Random rnd = new();
+
+        private readonly (string Question, string[] Options, int CorrectIndex)[] quizData = new[]
+        {
+            ("What is phishing?", new[] { "A fishing method", "Scam via email to steal info", "A virus", "A network tool" }, 1),
+            ("What does 2FA stand for?", new[] { "Two-Factor Authentication", "Fast Access", "Fake Account", "Firewall Alert" }, 0),
+            ("Why use a VPN?", new[] { "To watch Netflix", "For faster internet", "For secure internet connection", "To boost battery" }, 2),
+            ("Which is a strong password?", new[] { "123456", "password", "Pa$$w0rd123!", "abcdefg" }, 2),
+            ("What's the first thing to do when getting a suspicious email?", new[] { "Open it", "Click links", "Report or delete it", "Forward it" }, 2),
+            ("What is malware?", new[] { "A type of drink", "Malfunctioning hardware", "Malicious software", "An antivirus" }, 2),
+            ("What’s a good way to secure your account?", new[] { "Using your pet's name", "Writing your password on paper", "Enabling 2FA", "Never logging out" }, 2),
+            ("What is a firewall?", new[] { "Antivirus", "A password", "A security system", "A browser" }, 2),
+            ("What is the safest type of network?", new[] { "Public Wi-Fi", "Unsecured Home Network", "Secured Home Network", "Open Hotspot" }, 2),
+            ("What does HTTPS mean?", new[] { "It’s a virus", "It’s secure", "It’s a hacker tool", "It’s a browser" }, 1),
+        };
+
+        private static readonly Dictionary<string, List<string>> KeywordTips = new()
+        {
+            { "phishing", new List<string> {
+                "Beware of emails asking for personal information. Verify sender addresses.",
+                "Don't click suspicious links—hover to preview URLs.",
+                "Check for spelling errors and urgent messages—they're red flags!"
+            }},
+            { "password", new List<string> {
+                "Use a password manager to store complex, unique passwords.",
+                "Never reuse passwords across sites.",
+                "Use a mix of letters, numbers, and symbols."
+            }},
+            { "vpn", new List<string> {
+                "Use a VPN to secure your internet connection on public Wi-Fi.",
+                "Choose a reputable VPN provider with a no-logs policy.",
+                "Understand the limitations of VPNs—they don't make you invincible."
+            }},
+            { "malware", new List<string> {
+                "Keep your antivirus software updated.",
+                "Avoid downloading software from untrusted sources.",
+                "Regularly scan your devices for malware."
+            }},
+            { "privacy", new List<string> {
+                "Limit app permissions to only what's necessary.",
+                "Use privacy-focused browsers or extensions.",
+                "Review your social media privacy settings regularly."
+            }},
+            { "ransomware", new List<string> {
+                "Regularly back up your data to recover from ransomware attacks.",
+                "Be cautious of email attachments and links from unknown sources.",
+                "Use reputable security software to detect and block ransomware."
+            }},
+            { "firewall", new List<string> {
+                "A firewall monitors and controls incoming/outgoing traffic.",
+                "Firewalls protect against unauthorized access.",
+                "You should enable the firewall on all devices, including routers."
+            }},
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Schedet";
+            AskUserName();
+            PlayWelcomeSound();
+            reminderTimer.Start();
+        }
+
+        private void AskUserName()
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Welcome to Schedet!\nPlease enter your name:", "Schedet");
+            if (!string.IsNullOrWhiteSpace(input)) userName = input;
+        }
+
+        private void PlayWelcomeSound()
+        {
+            try
+            {
+                string filepath = @"C:\Users\lab_services_student\Desktop\PROG6221\PROG6221\SchedetBot\files\Greetings.wav";
+                SoundPlayer player = new SoundPlayer(filepath);
+                player.Play();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Voice file missing or not supported");
+            }
+        }
+
+        private void LogActivity(string description)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss");
+            activityLog.Add($"[{timestamp}] {description}");
+            if (activityLog.Count > 50) activityLog.RemoveAt(0);
+        }
+
+        private void chatbotSendButton_Click(object sender, EventArgs e)
+        {
+            string input = chatbotTextBox.Text.Trim();
+            chatbotTextBox.Clear();
+            chatbotOutput.AppendText($"> {userName}: {input}\n");
+
+            string lowerInput = input.ToLower();
+
+            // Greetings
+            if (Regex.IsMatch(lowerInput, @"\b(hi|hello|hey|howdy|good morning|good afternoon)\b"))
+            {
+                string[] greetings = { "Hi there!", "Hello 👋", "Greetings!", "Hey!", $"Welcome back, {userName}!" };
+                chatbotOutput.AppendText($"Bot 🤖: {greetings[rnd.Next(greetings.Length)]}\n");
+                LogActivity("Greeted user.");
+                return;
+            }
+
+            // Farewell
+            if (Regex.IsMatch(lowerInput, @"\b(bye|goodbye|see you|later|exit|quit)\b"))
+            {
+                string[] farewells = { "Goodbye! 👋", "Take care!", "Catch you later!", "Stay secure!", $"Goodbye, {userName}!" };
+                chatbotOutput.AppendText($"Bot 🤖: {farewells[rnd.Next(farewells.Length)]}\n");
+                LogActivity("User said goodbye.");
+                return;
+            }
+
+            // Navigation
+            if (lowerInput.Contains("show activity log"))
+            {
+                chatbotOutput.AppendText("Bot 📜: Here's your recent activity:\n");
+                foreach (var log in activityLog)
+                    chatbotOutput.AppendText("- " + log + "\n");
+                return;
+            }
+
+            if (lowerInput.Contains("quiz"))
+            {
+                tabControl.SelectedTab = quizTab;
+                chatbotOutput.AppendText("Bot 🤖: Switched to Quiz tab.\n");
+                LogActivity("Started quiz.");
+                LoadQuizQuestion();
+                return;
+            }
+
+            if (lowerInput.Contains("task"))
+            {
+                tabControl.SelectedTab = taskTab;
+                chatbotOutput.AppendText("Bot 📝: Opened Task Manager.\n");
+                return;
+            }
+
+            if (lowerInput.Contains("remind"))
+            {
+                tabControl.SelectedTab = reminderTab;
+                chatbotOutput.AppendText("Bot ⏰: Let's set a reminder.\n");
+                return;
+            }
+
+            // Sentiment
+            bool positive = Regex.IsMatch(lowerInput, @"\b(happy|confident|secure|good|love|great|excited)\b");
+            bool negative = Regex.IsMatch(lowerInput, @"\b(worried|scared|afraid|nervous|anxious|sad|unsafe|frustrated|angry)\b");
+
+            // Keywords
+            foreach (var keyword in KeywordTips.Keys)
+            {
+                if (lowerInput.Contains(keyword))
+                {
+                    var tips = KeywordTips[keyword];
+                    string tip = tips[rnd.Next(tips.Count)];
+
+                    string[] prefixPositive = {
+                        "That's awesome to hear! Here’s something extra to consider: ",
+                        "You're on the right track! Here's a helpful tip: ",
+                        "Excellent attitude! Let me add this: "
+                    };
+
+                    string[] prefixNegative = {
+                        "It's okay to feel that way. Here's a suggestion: ",
+                        "Don't worry, you're not alone. Try this: ",
+                        "Stay calm and check this out: "
+                    };
+
+                    string prefix = positive ? prefixPositive[rnd.Next(prefixPositive.Length)] :
+                                    negative ? prefixNegative[rnd.Next(prefixNegative.Length)] : "";
+
+                    chatbotOutput.AppendText($"Bot 💡: {prefix}{tip}\n");
+                    LogActivity($"Asked about '{keyword}'");
+                    return;
+                }
+            }
+
+            // Sentiment-only response
+            if (positive)
+            {
+                string[] responses = {
+                    "Glad to hear that! 😊",
+                    "Stay positive and secure!",
+                    "That’s great! Let me know if you need any help."
+                };
+                chatbotOutput.AppendText($"Bot 🤗: {responses[rnd.Next(responses.Length)]}\n");
+                return;
+            }
+
+            if (negative)
+            {
+                string[] responses = {
+                    "I understand you're not feeling the best. Let's focus on your digital safety.",
+                    "If something's bothering you, you can talk to someone you trust.",
+                    "I'm here to help—would you like a cybersecurity tip?"
+                };
+                chatbotOutput.AppendText($"Bot 🙁: {responses[rnd.Next(responses.Length)]}\n");
+                return;
+            }
+
+            // Fallback
+            string[] fallbackResponses = {
+                "Hmm... Try keywords like 'password', 'VPN', 'phishing', or ask to 'show activity log'.",
+                "I'm still learning! Can you rephrase that or use a keyword like 'malware' or 'privacy'?",
+                "Sorry, I didn’t quite get that. You could ask about 'ransomware', '2FA', or 'firewalls'."
+            };
+            chatbotOutput.AppendText($"Bot 🤖: {fallbackResponses[rnd.Next(fallbackResponses.Length)]}\n");
+        }
+
+        private void LoadQuizQuestion()
+        {
+            if (currentQuestion >= quizData.Length)
+            {
+                MessageBox.Show($"🎉 Quiz complete! You scored {score}/{quizData.Length}.", "Quiz Results");
+                currentQuestion = 0;
+                score = 0;
+                return;
+            }
+
+            var question = quizData[currentQuestion];
+            quizLabel.Text = question.Question;
+            quizOption1.Text = question.Options[0];
+            quizOption2.Text = question.Options[1];
+            quizOption3.Text = question.Options[2];
+            quizOption4.Text = question.Options[3];
+
+            quizOption1.Checked = quizOption2.Checked = quizOption3.Checked = quizOption4.Checked = false;
+        }
+
+        private void submitQuizButton_Click(object sender, EventArgs e)
+        {
+            int selected = quizOption1.Checked ? 0 :
+                           quizOption2.Checked ? 1 :
+                           quizOption3.Checked ? 2 :
+                           quizOption4.Checked ? 3 : -1;
+
+            if (selected == -1)
+            {
+                MessageBox.Show("Please select an answer.");
+                return;
+            }
+
+            string correctAnswer = quizData[currentQuestion].Options[quizData[currentQuestion].CorrectIndex];
+            if (selected == quizData[currentQuestion].CorrectIndex)
+            {
+                MessageBox.Show("✅ Correct!");
+                score++;
+            }
+            else
+            {
+                MessageBox.Show($"❌ Incorrect. Correct answer: {correctAnswer}");
+            }
+
+            questionSummaryListBox.Items.Add($"{quizData[currentQuestion].Question} — Answer: {correctAnswer}");
+            LogActivity($"Answered quiz question: {quizData[currentQuestion].Question}");
+            currentQuestion++;
+            LoadQuizQuestion();
+        }
+
+        private void addTaskButton_Click(object sender, EventArgs e)
+        {
+            string task = $"Name: {taskNameTextBox.Text} | Desc: {taskDescTextBox.Text} | Time: {taskTimeTextBox.Text} | Status: {taskStatusComboBox.Text}";
+            taskListBox.Items.Add(task);
+            LogActivity($"Added task: {taskNameTextBox.Text}");
+            ClearTaskFields();
+        }
+
+        private void editTaskButton_Click(object sender, EventArgs e)
+        {
+            int index = taskListBox.SelectedIndex;
+            if (index != -1)
+            {
+                string task = $"Name: {taskNameTextBox.Text} | Desc: {taskDescTextBox.Text} | Time: {taskTimeTextBox.Text} | Status: {taskStatusComboBox.Text}";
+                taskListBox.Items[index] = task;
+                LogActivity($"Edited task: {taskNameTextBox.Text}");
+                ClearTaskFields();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to edit.");
+            }
+        }
+
+        private void deleteTaskButton_Click(object sender, EventArgs e)
+        {
+            int index = taskListBox.SelectedIndex;
+            if (index != -1)
+            {
+                string task = taskListBox.Items[index].ToString();
+                taskListBox.Items.RemoveAt(index);
+                LogActivity($"Deleted task: {task}");
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.");
+            }
+        }
+
+        private void taskListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (taskListBox.SelectedIndex != -1)
+            {
+                string[] parts = taskListBox.SelectedItem.ToString().Split('|');
+                taskNameTextBox.Text = GetValue(parts, "Name:");
+                taskDescTextBox.Text = GetValue(parts, "Desc:");
+                taskTimeTextBox.Text = GetValue(parts, "Time:");
+                taskStatusComboBox.Text = GetValue(parts, "Status:");
+            }
+        }
+
+        private string GetValue(string[] parts, string key)
+        {
+            foreach (var part in parts)
+                if (part.Trim().StartsWith(key))
+                    return part.Substring(part.IndexOf(":") + 1).Trim();
+            return "";
+        }
+
+        private void ClearTaskFields()
+        {
+            taskNameTextBox.Clear();
+            taskDescTextBox.Clear();
+            taskTimeTextBox.Clear();
+            taskStatusComboBox.SelectedIndex = -1;
+        }
+
+        private void SetReminderButton_Click(object sender, EventArgs e)
+        {
+            string input = reminderTextBox.Text;
+            var match = Regex.Match(input, @"(.+)\s+at\s+(\d{2}:\d{2})");
+            if (match.Success)
+            {
+                string message = match.Groups[1].Value.Trim();
+                string time = match.Groups[2].Value.Trim();
+                reminders.Add((time, message));
+                MessageBox.Show($"Reminder set for {time}: {message}");
+                LogActivity($"Reminder set: '{message}' at {time}");
+            }
+            else
+            {
+                MessageBox.Show("Invalid format. Use: 'Drink water at 14:30'");
+            }
+        }
+
+        private void ReminderTimer_Tick(object sender, EventArgs e)
+        {
+            string now = DateTime.Now.ToString("HH:mm");
+            foreach (var (time, message) in reminders)
+            {
+                if (now == time)
+                {
+                    MessageBox.Show($"🔔 Reminder: {message}");
+                }
+            }
+        }
+    }
+}
